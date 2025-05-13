384ebc9f2fc1a0cb5b14ce7cd4adc6ceccc71db0
qb-le <quocbaole2103@gmail.com>
2025-05-13 10:20:58 +0200

    Project created, Database connected

---
+++ LunchList/Controllers/ItemsControllers.cs
@@ -0,0 +1,24 @@
+﻿using LunchList.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LunchList.Controllers
+{
+    public class GroceryListController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        // Constructor to inject DbContext
+        public GroceryListController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /GroceryList
+        public IActionResult Index()
+        {
+            var groceryItems = _context.GroceryItems.ToList();
+            return View(groceryItems); 
+        }
+    }
+}
+
---
+++ LunchList/Data/AppDBContext.cs
@@ -0,0 +1,19 @@
+﻿using LunchList.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace LunchList.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<GroceryItem> GroceryItems { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<GroceryItem>().ToTable("grocery_lists");
+        }
+    }
+}
