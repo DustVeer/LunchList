df528c920640a5c3b32bb62ace58af633cd82fcb
Liam4404S <553714@student.fontys.nl>
2025-05-28 16:01:00 +0200

    history details page added and complete.

--- LunchList/Controllers/HistoryController.cs
+++ LunchList/Controllers/HistoryController.cs
@@ -17,4 +17,42 @@ public class HistoryController(AppDbContext context) : Controller
            
         return View(groceryLists);
     }
+
+    public async Task<IActionResult> Details(int id)
+    {
+        GroceryList? groceryList = await _context.GroceryLists
+            .FirstOrDefaultAsync(gl => gl.Id == id);
+
+        if (groceryList == null)
+        {
+            TempData["ShowModal"] = true;
+            return View(new GroceryListViewModel());
+        }
+
+
+
+        var products = await _context.GroceryListItems
+            .Where(gli => gli.GroceryListId == groceryList.Id)
+            .Include(gli => gli.GroceryItem)
+            .ThenInclude(gi => gi.RetailerProduct)
+            .Select(gli => new GroceryListViewModelProducts
+            {
+                GroceryItem = gli.GroceryItem,
+                RetailerProduct = gli.GroceryItem.RetailerProduct,
+                Price = gli.GroceryItem.RetailerProduct.PricePerProduct * gli.GroceryItem.Quantity
+            })
+            .ToListAsync();
+
+        var totalListPrice = products.Sum(item => item.GroceryItem.RetailerProduct.PricePerProduct * item.GroceryItem.Quantity);
+
+        var model = new GroceryListViewModel()
+        {
+            Id = groceryList.Id,
+            Name = groceryList.Name,
+            GroceryListViewModelProducts = products,
+            TotalPrice = totalListPrice
+        };
+
+        return View(model);
+    }
 }
